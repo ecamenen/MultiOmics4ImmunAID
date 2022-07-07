2b2c6b075e84ecb62782b7590a8283c26e251e8e
ecamenen <etienne.camenen@gmail.com>
2022-07-07 12:25:23 +0200

    Add vignette to parse

--- R/MultiOmics4ImmunAID-package.R
+++ R/MultiOmics4ImmunAID-package.R
@@ -1,3 +1,5 @@
-#' @import magrittr openxlsx
+#' @import golem janitor openxlsx stringr tidyverse venn
+#' @importFrom magrittr %>%
+#' @rawNamespace import(gplots, except = venn)
 #' @keywords internal
 "_PACKAGE"
