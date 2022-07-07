f825d1944b70537479ec1ed4ec1c1f64df54de00
ecamenen <etienne.camenen@gmail.com>
2022-07-07 17:59:48 +0200

    Add more data man to elisa part

--- R/MultiOmics4ImmunAID-package.R
+++ R/MultiOmics4ImmunAID-package.R
@@ -1,5 +1,7 @@
-#' @import golem janitor openxlsx stringr tidyverse venn
+#' @import DataExplorer dlookr golem janitor naniar readxl reshape2
+#' @import stringr tidyverse venn VIM
 #' @importFrom magrittr %>%
 #' @rawNamespace import(gplots, except = venn)
+#' @rawNamespace import(skimr, except = n_complete)
 #' @keywords internal
 "_PACKAGE"
