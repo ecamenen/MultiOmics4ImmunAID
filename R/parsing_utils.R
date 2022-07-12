0c10f0ee16312c93e930b2febf68601d24acec17
ecamenen <etienne.camenen@gmail.com>
2022-07-12 11:11:51 +0200

    Formate codes

--- R/parsing_utils.R
+++ R/parsing_utils.R
@@ -1,22 +1,43 @@
+
+
+clean_data <- function(x,
+                       clean_name = TRUE,
+                       row_number = 1) {
+    if (clean_name) {
+        x <- clean_names(x)
+    }
+    x %>%
+        remove_empty(c("rows", "cols"), quiet = FALSE) %>%
+        remove_constant(na.rm = TRUE, quiet = FALSE)
+}
+
 get_name_num <- function(x) {
-  colnames(x)[
-    sapply(
-      colnames(x),
-      function(i) is.numeric(c(x[, i])[[1]])
-    )
-  ]
+    colnames(x)[
+      sapply(
+        colnames(x),
+        function(i) {
+              is.numeric(c(x[, i])[[1]])
+          }
+    )]
 }
 
 get_var_names <- function(x) {
-  (stats %>% filter(str_detect(column_code, paste0(c(x), collapse = "|"))))$item_name
+    (stats %>%
+       filter(
+         str_detect(
+           column_code,
+           paste0(c(x), collapse = "|")
+           )
+         )
+     )$item_name
 }
 
 get_melt <- function(x) {
-  (x %>% select(all_of(get_name_num(x))) %>% gather())
+    (x %>% select(all_of(get_name_num(x))) %>% gather())
 }
 
 get_intersection <- function(x) {
-  res <- gplots::venn(x)
-  intersections <- attributes(res)$intersections
-  intersections[grepl(":", names(intersections))]
+    res <- gplots::venn(x)
+    intersections <- attributes(res)$intersections
+    intersections[grepl(":", names(intersections))]
 }
--- R/stats_utils.R
+++ R/stats_utils.R
@@ -1,73 +1,86 @@
 best_na_percent <- function(x, threshold = 50) {
-  (df_na <- diagnose(x) %>% filter(missing_percent > threshold) %>% arrange(desc(missing_percent)))
-  var_na <- data.frame(df_na %>% select(variables))[, 1]
-  x %>% select(-one_of(var_na))
+    (df_na <- diagnose(x) %>%
+       filter(missing_percent > threshold) %>%
+       arrange(desc(missing_percent))
+     )
+    var_na <- data.frame(df_na %>% select(variables))[, 1]
+    x %>% select(-one_of(var_na))
 }
 
 get_correlation <- function(x, threshold = 0.5, half = TRUE) {
-  y <- correlate(x) %>%
-    filter(abs(coef_corr) > threshold)
-  if (half) {
-    y %>% filter(as.integer(var1) > as.integer(var2))
-  } else {
-    y
-  }
+    y <- correlate(x) %>%
+        filter(abs(coef_corr) > threshold)
+    if (half) {
+        y %>% filter(as.integer(var1) > as.integer(var2))
+    } else {
+        y
+    }
 }
 
 get_corr <- function(x, pval = FALSE) {
-  x <- as.data.frame(x)
-  vars <- colnames(x)[sapply(colnames(x), function(i) is.numeric(x[, i]))]
-  sapply(
-    vars,
-    function(i) {
+    x <- as.data.frame(x)
+    vars <- colnames(x)[
       sapply(
-        vars,
-        function(j) {
-          if (is.numeric(x[, i]) & is.numeric(x[, j])) {
-            res <- cor.test(x[, i], x[, j])
-            if (pval) {
-              res$estimate
-            } else {
-              res$p.value
-            }
-          } else {
-            NA
-          }
-        }
+        colnames(x),
+        function(i) { is.numeric(x[, i]) }
       )
-    }
-  )
+    ]
+    sapply(
+        vars,
+        function(i) {
+            sapply(
+                vars,
+                function(j) {
+                    if (is.numeric(x[, i]) & is.numeric(x[, j])) {
+                        res <- cor.test(x[, i], x[, j])
+                        if (pval) {
+                            res$estimate
+                        } else {
+                            res$p.value
+                        }
+                    } else {
+                        NA
+                    }
+                }
+            )
+        }
+    )
 }
 
 explore_outliers <- function(x) {
-  colnames(x)[find_outliers(x)]
+    colnames(x)[find_outliers(x)]
 }
 
-get_outliers <- function(x, y, probs = c(0.25, 0.75), method = "iqr", c = 1.5) {
-  stopifnot(method %in% c("iqr", "percentiles", "hampel", "mad", "sd"))
-  y <- as.data.frame(x)[, y]
-  # outliers <- boxplot(y)$out
-  # percentiles: probs = c(0.025, 0.975)
-  Q <- quantile(y, probs = probs, na.rm = TRUE)
-  up <- Q[2]
-  low <- Q[1]
-  if (method == "iqr") {
-    # interquartile range: 1.5 * IQR
-    iqr <- IQR(y, na.rm = TRUE)
-    up <- up + c * iqr
-    low <- low - c * iqr
-  }
-  if (method %in% c("hampel", "mad", "sd")) {
-    if (method %in% c("hampel", "mad")) {
-      # mediane absolute deviation: 3 * MAD
-      med <- median(y, na.rm = TRUE)
-      mad3 <- c * mad(y, na.rm = TRUE, constant = 1)
-    } else {
-      med <- median(y, na.rm = TRUE)
-      mad3 <- c * sd(y, na.rm = TRUE)
+get_outliers <- function(
+  x,
+             y,
+             probs = c(0.25, 0.75),
+             method = "iqr",
+             c = 1.5) {
+        stopifnot(method %in% c("iqr", "percentiles", "hampel", "mad", "sd"))
+        y <- as.data.frame(x)[, y]
+        # outliers <- boxplot(y)$out
+        # percentiles: probs = c(0.025, 0.975)
+        Q <- quantile(y, probs = probs, na.rm = TRUE)
+        up <- Q[2]
+        low <- Q[1]
+        if (method == "iqr") {
+            # interquartile range: 1.5 * IQR
+            iqr <- IQR(y, na.rm = TRUE)
+            up <- up + c * iqr
+            low <- low - c * iqr
+        }
+        if (method %in% c("hampel", "mad", "sd")) {
+            if (method %in% c("hampel", "mad")) {
+                # mediane absolute deviation: 3 * MAD
+                med <- median(y, na.rm = TRUE)
+                mad3 <- c * mad(y, na.rm = TRUE, constant = 1)
+            } else {
+                med <- median(y, na.rm = TRUE)
+                mad3 <- c * sd(y, na.rm = TRUE)
+            }
+            up <- med + mad3
+            low <- med - mad3
+        }
+        x %>% filter(y < low | y > up)
     }
-    up <- med + mad3
-    low <- med - mad3
-  }
-  x %>% filter(y < low | y > up)
-}
