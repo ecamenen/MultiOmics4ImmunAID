c0ccdc6bace13de107394ada93a5278ac14905dc
ecamenen <etienne.camenen@gmail.com>
2022-08-03 10:10:12 +0200

    Create blocks

---
+++ dev/create_blocks.R
@@ -0,0 +1,93 @@
+path <- file.path(get_golem_wd(), "data")
+files <- list.files(path)[!str_detect(list.files(path), "blocks|behavioral|code")] %>%
+    sort() %>%
+    str_replace_all(".rda", "")
+
+for (f in files) {
+    load(file.path(get_golem_wd(), "data", paste0(f, ".rda")))
+}
+
+blocks <- lapply(files, function(x) as.data.frame(get(x)))
+names(blocks) <- toupper(files)
+
+# Keep only the level A and rename the rows
+omics <- c("ELISA", "MS", "RNA")
+for (i in omics) {
+    blocks[[i]] <- filter(blocks[[i]], detect_row(blocks[[i]], "A"))
+    rownames(blocks[[i]]) <- get_patient_id(blocks[[i]])
+}
+
+blocks <- blocks[c("MS", "RNA", "CLINIC")]
+common_rows <- Reduce(intersect, lapply(blocks, row.names))
+blocks <- lapply(blocks, function(x) clean_data(x[common_rows, ], FALSE))
+
+disease <- c("Still", "osteitis")
+# Select the numerical variables
+blocks[["ELISA"]] <- select(blocks[["ELISA"]], c(4, 5))
+levels(blocks[["CLINIC"]]$gender) <- c(0, 1)
+blocks[["CLINIC"]] <- get_name_num(blocks[["CLINIC"]]) %>%
+    select(blocks[["CLINIC"]], .)
+
+# All cofonding variables in numeric
+blocks[["CLINIC"]]$gender <- as.numeric(as.character(blocks[["CLINIC"]]$gender))
+row_names <- rownames(blocks[["CLINIC"]])
+# Transform
+blocks[["CLINIC"]][-seq(6), ] <- as.data.frame(sapply(blocks[["CLINIC"]][-seq(6), ], log))
+blocks[["CLINIC"]][blocks[["CLINIC"]] == "-Inf"] <- 0
+rownames(blocks[["CLINIC"]]) <- row_names
+
+# Remove missing samples from cofonding variables
+vars <- c("gender", "age_at_inclusion_tim", "bmi_automatic")
+to_remove <- sapply(vars, function(i) which(is.na(blocks[["CLINIC"]][, i])))
+to_remove <- unique(Reduce(c, to_remove))
+blocks <- lapply(blocks, function(i) i[-to_remove, ])
+cl <- blocks[["CLINIC"]]
+
+# Weight by the cofunding effect residuals
+blocks.df <- lapply(
+    blocks,
+    function(x) {
+        lapply(
+            seq(ncol(x)),
+            function(y) {
+                  lm(x[, y] ~ cl$age_at_inclusion_tim + cl$bmi_automatic, na.action = "na.exclude")$residuals
+              }
+        )
+    }
+)
+
+# Position of the NA values in blocks
+for (k in seq(length(blocks))) {
+    listNA <- which(is.na(blocks[[k]]), arr.ind = TRUE)
+
+    # Insert NA
+    for (i in unique(listNA[, 2])) {
+        for (j in listNA[which(listNA[, 2] == i), 1] - 1) {
+              blocks.df[[k]][[i]] <- append(
+                  blocks.df[[k]][[i]],
+                  NA,
+                  j
+              )
+          }
+    }
+
+    # Convert in matrix
+    blocks.df[[k]] <- as.data.frame(
+        matrix(
+            unlist(blocks.df[[k]]),
+            nrow = nrow(blocks[[k]]),
+            ncol = ncol(blocks[[k]]),
+            dimnames = list(row.names(blocks[[k]]), colnames(blocks[[k]]))
+        )
+    )
+}
+blocks <- blocks.df
+blocks[["CLINIC"]] <- select(blocks[["CLINIC"]], -seq(6)) # %>%
+# filter(str_detect(blocks[["CLINIC"]]$disease, disease[1]))
+
+blocks <- lapply(blocks, function(x) clean_data(x, FALSE))
+# blocks_w_ms <- blocks
+use_data(blocks, overwrite = TRUE)
+
+# plot_violin(blocks[["CLINIC"]] %>% select(1:4), colors = rep("blue", 200))
+# plot_network(blocks, colors =  c("#eee685", "white"))
