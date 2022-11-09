1cd70cd9637323e7c12ac50a405e5bc24934bddf
ecamenen <etienne.camenene@gmail.com>
2022-11-09 17:17:11 +0100

    Add plot_violin and plot_piechart

--- R/plot_violin.R
+++ R/plot_violin.R
@@ -1,4 +1,40 @@
 #' @export
+plot_piechart <- function(x, hsize = 1.2, cex = 15, colour = get_colors(), wrap = 5, lwd = 4) {
+    df <- fct_drop(x) %>%
+        fct_infreq() %>%
+        fct_count() %>%
+        mutate(
+            hsize = hsize,
+            pos = rev(cumsum(rev(n))),
+            pos = n / 2 + lead(pos, 1),
+            pos = if_else(is.na(pos), n / 2, pos),
+            label = str_wrap(str_glue("{f} (N={n})"), wrap)
+        )
+    ggplot(df, aes(x = hsize, y = n, fill = f)) +
+        geom_col(width = .8, color = "white", lwd = lwd) +
+        geom_text(
+            color = "white",
+            size = cex / 2.5,
+            aes(label = scales::percent(n / sum(n), .1)),
+            position = position_stack(vjust = 0.5)
+        ) +
+        coord_polar(theta = "y", clip = "off") +
+        scale_fill_manual(values = colour, na.value = "black") +
+        scale_y_continuous(breaks = df$pos, labels = df$label) +
+        guides(fill = guide_legend(title = "Group")) +
+        theme(
+            axis.ticks = element_blank(),
+            axis.title = element_blank(),
+            axis.text.x = element_text(size = cex),
+            axis.text.y = element_blank(),
+            legend.position = "none",
+            panel.background = element_rect(fill = "white"),
+            plot.margin = unit(c(0, -1, -1, -1), "cm")
+        ) +
+        xlim(0.5, hsize + 0.5)
+}
+
+#' @export
 plot_histo <- function(
     x,
     colors = get_colors(),
@@ -80,6 +116,46 @@ plot_violin1 <- function(
     theme_violin(
         p,
         colors = colors,
+        cex = cex,
+        cex_main = cex_main,
+        cex_sub = cex_sub
+    )
+}
+
+#' @export
+plot_violin0 <- function(
+    x,
+    colour = "red",
+    lwd = 1,
+    size = 1,
+    cex = 1,
+    cex_main = 12 * cex,
+    cex_sub = 10 * cex) {
+    df <- data.frame(value = x, name = 1)
+    m <- median(df$value, na.rm = TRUE)
+    iqr <- 1.5 * IQR(df$value, na.rm = TRUE)
+    p <- ggplot(df, aes(x = name, y = value)) +
+        geom_errorbar(
+            width = .1,
+            lwd = lwd,
+            colour = colour,
+            aes(
+                ymin = m - iqr,
+                ymax = m + iqr
+            )
+        ) +
+        geom_boxplot(
+            coef = 0,
+            outlier.shape = NA,
+            colour = "white",
+            fill = colour,
+            lwd = lwd
+        ) +
+        geom_violin(alpha = 0.1, fill = colour, colour = NA) +
+        geom_sina(size = size) +
+        theme_bw()
+    theme_violin1(
+        p,
         cex = cex,
         cex_main = cex_main,
         cex_sub = cex_sub
