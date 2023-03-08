84dcec2485bf0e8c1f28400e4433211c5d15d1a8
Etienne Camenen <etienne.camenen@gmail.com>
2023-03-08 12:07:33 +0100

    Add colors for backgrounds, header font, fix replacement

--- inst/excel4treatments.py
+++ inst/excel4treatments.py
@@ -1,10 +1,21 @@
 import openpyxl
 import argparse
+from openpyxl.styles import Alignment, PatternFill, Font
+
+get_color = ["c1bcbc", "ffd92f", "76cf73", "519fd5", "ef6e69"]
 
 # Define the columns that we want to merge cells in
 columns_to_merge = ["ID", "Disease", "N previous treatment", "Date of the 1st visit",
                     "Date of the 2nd visit", "Date of the follow-up", "Eligible for a 2nd visit"]
 
+# Define the colors to use for each column
+column_colors = {
+    "A:B": get_color[0],
+    "C:P": get_color[1],
+    "Q:AF": get_color[2],
+    "AG:AR": get_color[3],
+    "AS:BD": get_color[4]
+}
 
 # Define the fill pattern to use for highlighting cells containing "Missing"
 missing_fill = PatternFill(start_color='FFFF00', end_color='FFFF00', fill_type='solid')
@@ -13,7 +24,7 @@ missing_fill = PatternFill(start_color='FFFF00', end_color='FFFF00', fill_type='
 def merge_empty_cells(column):
     # Initialize variables to keep track of the first non-empty cell
     first_non_empty_cell = None
-    
+
     # Iterate over all cells in the column
     for cell in column:
         # If the cell is empty and we haven't found a non-empty cell yet, skip it
@@ -29,7 +40,7 @@ def merge_empty_cells(column):
         # If the cell is not empty, record it as the first non-empty cell
         else:
             first_non_empty_cell = cell
-            
+
 # Parse the filename argument from the command line
 parser = argparse.ArgumentParser()
 parser.add_argument("filename", help="the name of the Excel file to merge empty cells in")
@@ -40,21 +51,34 @@ wb = openpyxl.load_workbook(args.filename)
 
 # Iterate over all sheets in the workbook
 for sheet in wb:
+    # Color each column with the corresponding color
+    for range_str, color in column_colors.items():
+        for cell in sheet[range_str]:
+            if isinstance(cell, tuple):
+                cell_list = cell
+            else:
+                cell_list = [cell]
+            for c in cell_list:
+                c.fill = PatternFill(fgColor=color, fill_type="solid")
+
     # Iterate over all columns in the sheet
     for col in sheet.columns:
         # If the column is one we want to merge cells in, merge the empty cells
         if col[0].value in columns_to_merge:
             merge_empty_cells(col)
 
-    for cell in col:
         # Highlight cells containing the word "Missing" in yellow
-        if cell.value == "Missing":
-            cell.fill = missing_fill
-        # Replace any cell containing "NA" with an empty string
-        if cell.value == "NA":
-            cell.value = ""
+        for cell in col:
+            if cell.value == "Missing":
+                cell.fill = missing_fill
+            # Replace any cell containing "NA" with an empty string
+            if cell.value == "NA":
+                cell.value = ""
+
+
+    # Make the header row bold
+    for cell in sheet["1"]:
+        cell.font = Font(name='Calibri', bold=True, italic=True)
 
 # Save the modified Excel file with the same name as the original file
 wb.save(args.filename)
-
-
